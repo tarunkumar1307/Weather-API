18f018ce16e923b4c0ad63c5f2c97bdf90203fa7
Aadi <sskumartarun@gmail.com>
2025-04-08 10:35:58 +0530

    Weather API Pushed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './component/weather'
+
+const App = () => {
+  return (
+    <div className = "app">
+      <Weather />
+    </div>
+  )
+}
+
+export default App
